cc6c28aeda4c3fbefdeb6451dc33dc8b2d4257e9
llky <lesterlohky@gmail.com>
2022-12-22 00:50:41 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,69 @@
+import { createBoard, playMove } from "./connect4.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const board = document.querySelector(".board");
+  createBoard(board);
+  const websocket = new WebSocket("ws://websockets-zyf3jml75q-uc.a.run.app");
+  initGame(websocket);
+  receiveMoves(board, websocket);
+  sendMoves(board, websocket);
+});
+
+function sendMoves(board, websocket) {
+  board.addEventListener("click", ({ target }) => {
+    const column = target.dataset.column;
+
+    if (column === undefined) {
+      return;
+    }
+    const event = {
+      type: "play",
+      column: parseInt(column),
+    };
+    websocket.send(JSON.stringify(event));
+  });
+}
+
+function showMessage(message) {
+  window.setTimeout(() => window.alert(message), 50);
+}
+
+function receiveMoves(board, websocket) {
+  websocket.addEventListener("message", ({ data }) => {
+    const event = JSON.parse(data);
+    switch (event.type) {
+      case "init":
+        document.querySelector(".join").href = "?join=" + event.join;
+        document.querySelector(".watch").href = "?watch=" + event.watch;
+        break;
+      case "play":
+        playMove(board, event.player, event.column, event.row);
+        break;
+      case "win":
+        showMessage(`Player ${event.player} wins!`);
+        websocket.close(1000);
+        break;
+      case "error":
+        showMessage(event.message);
+        break;
+      default:
+        throw new Error(`Unsupported event type: ${event.type}.`);
+    }
+  });
+}
+
+function initGame(websocket) {
+  websocket.addEventListener("open", () => {
+    const params = new URLSearchParams(window.location.search);
+    const event = { type: "init" };
+    if (params.has("join")) {
+      // Second player joins an existing game.
+      event.join = params.get("join");
+    } else if (params.has("watch")) {
+      event.watch = params.get("watch");
+    } else {
+      // First player starts a new game.
+    }
+    websocket.send(JSON.stringify(event));
+  });
+}
